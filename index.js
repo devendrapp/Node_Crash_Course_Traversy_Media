848ea18d0698b83fdd37d8a0d9b7999592f1cf18
Devendra Panchwagh <devendrapp@gmail.com>
2023-12-22 08:21:53 +0530

    22-Dec

---
+++ index.js
@@ -0,0 +1,78 @@
+const http = require('http');
+const path = require('path');
+const fs = require('fs');
+
+const PORT = process.env.PORT || 8081;
+const server = http.createServer((req,res)=>{
+    console.log(req.url);
+
+    // if(req.url === '/'){
+    //     fs.readFile(path.join(__dirname,'public','index.html'), (err,content) =>{
+    //         if(err) throw err;
+    //         res.writeHead(200,{'Content-Type': 'text/html'});
+    //         res.end(content);
+    //     });
+    // }
+
+    // if(req.url === '/about'){
+    //     fs.readFile(path.join(__dirname,'public','about.html'), (err,content) =>{
+    //         if(err) throw err;
+    //         res.writeHead(200,{'Content-Type': 'text/html'});
+    //         res.end(content);
+    //     });
+    // }
+    
+    // if(req.url ==='/api/users'){
+    //     const users =[
+    //         {name: 'Amit', age: 30},
+    //         {name: 'Ramesh', age: 20},
+    //     ];
+    //     res.writeHead(200,{'Content-Type': 'application/json'});
+    //     res.end(JSON.stringify(users));
+    // }
+
+    let filePath=path.join(__dirname,'public',req.url === '/' ? 'index.html':req.url);
+    //Extension of file
+    let extName=path.extname(filePath);
+    let contentType='text/html';
+    switch(extName){
+        case '.js':
+            contentType='text/javascript';
+            break;
+        case '.css':
+            contentType='text/css';
+            break;
+        case '.json':
+            contentType='application/json';
+            break;
+        case '.png':
+            contentType='image/png';
+            break;            
+        case '.js':
+            contentType='image/jpg';
+            break;
+    }
+
+    //Read File
+    fs.readFile(filePath, (err,content)=> {
+        if(err){
+            console.log(err.code);
+            if(err.code === 'ENOENT'){
+                fs.readFile(path.join(__dirname,'public','404.html')
+                            ,(err,content) => {
+                                res.writeHead(404,{'Content-Type': 'text/html'});
+                                res.write(content,'utf-8');
+                            });
+            }else{
+                res.writeHead(500);
+                res.end(`Server Error: ${err.code}`);
+            }
+        }
+        res.writeHead(200,{'Content-Type': contentType});
+        res.end(content,'utf-8');
+
+    });
+    console.log(filePath);    
+});
+
+server.listen(PORT, () => console.log(`Server running on ${PORT}`));
